40b9896bd1bb5991d5e3dae556372bc17c46fd5b
iohnS <john02sun@gmail.com>
2022-10-15 14:29:37 +0200

    Fixed sorting

--- src/components/tables/MemberTable.tsx
+++ src/components/tables/MemberTable.tsx
@@ -51,6 +51,25 @@ const MemberTable = () => {
     };
   }, []);
 
+  function compareStrings(a: string, b: string, type: string) {
+    let i = 0;
+    try {
+      while (a.charCodeAt(i) == b.charCodeAt(0)) {
+        i += 1;
+      }
+    } catch (e) {
+      console.log(e);
+    }
+
+    const x = a.charCodeAt(i);
+    const y = b.charCodeAt(i);
+    if (type === "asc") {
+      return x - y;
+    } else {
+      return y - x;
+    }
+  }
+
   function sortData(data: DocumentData[] | undefined) {
     if (!data) {
       return;
@@ -67,19 +86,15 @@ const MemberTable = () => {
         } else if (sortColumn === "ssn") {
           x = parseInt(x.split("-")[0]);
           y = parseInt(y.split("-")[0]);
-        } else {
-          if (typeof x === "string") {
-            x = x.charCodeAt(0);
-          }
-          if (typeof y === "string") {
-            y = y.charCodeAt(0);
-          }
         }
-
-        if (sortType === "asc") {
-          return x - y;
+        if (typeof x === "string" && typeof y === "string") {
+          return compareStrings(x, y, sortType);
         } else {
-          return y - x;
+          if (sortType == "asc") {
+            return x - y;
+          } else {
+            return y - x;
+          }
         }
       });
     }
@@ -228,105 +243,90 @@ const MemberTable = () => {
   }
 
   return (
-    <>
-      <Table
-        virtualized
-        data={sortData(dbData)}
-        height={420}
-        sortColumn={sortColumn}
-        sortType={sortType}
-        onSortColumn={handleSortColumn}
-        loading={loading}
-      >
-        <Column width={250} align="center" fixed sortable resizable>
-          <HeaderCell>Id</HeaderCell>
-          <Cell dataKey="id" />
-        </Column>
+    <Table
+      virtualized
+      data={sortData(dbData)}
+      height={420}
+      sortColumn={sortColumn}
+      sortType={sortType}
+      onSortColumn={handleSortColumn}
+      loading={loading}
+    >
+      <Column width={250} align="center" fixed sortable resizable>
+        <HeaderCell>Id</HeaderCell>
+        <Cell dataKey="id" />
+      </Column>
 
-        <Column width={130} fixed sortable resizable>
-          <HeaderCell>Name</HeaderCell>
-          <EditableCell
-            rowData
-            dataKey="name"
-            type="form"
-            onChange={handleChange}
-          />
-        </Column>
+      <Column width={130} fixed sortable resizable>
+        <HeaderCell>Name</HeaderCell>
+        <EditableCell
+          rowData
+          dataKey="name"
+          type="form"
+          onChange={handleChange}
+        />
+      </Column>
 
-        <Column width={200} sortable resizable>
-          <HeaderCell>Email</HeaderCell>
-          <EditableCell
-            rowData
-            dataKey="email"
-            type="form"
-            onChange={handleChange}
-          />
-        </Column>
+      <Column width={200} sortable resizable>
+        <HeaderCell>Email</HeaderCell>
+        <EditableCell
+          rowData
+          dataKey="email"
+          type="form"
+          onChange={handleChange}
+        />
+      </Column>
 
-        <Column width={200} sortable resizable>
-          <HeaderCell>SSN</HeaderCell>
-          <EditableCell
-            rowData
-            dataKey="ssn"
-            type="form"
-            onChange={handleChange}
-          />
-        </Column>
+      <Column width={200} sortable resizable>
+        <HeaderCell>SSN</HeaderCell>
+        <EditableCell
+          rowData
+          dataKey="ssn"
+          type="form"
+          onChange={handleChange}
+        />
+      </Column>
 
-        <Column width={200} sortable resizable>
-          <HeaderCell>Registration date</HeaderCell>
-          <EditableCell
-            rowData
-            dataKey="regDate"
-            type="form"
-            onChange={handleChange}
-          />
-        </Column>
+      <Column width={200} sortable resizable>
+        <HeaderCell>Registration date</HeaderCell>
+        <EditableCell
+          rowData
+          dataKey="regDate"
+          type="form"
+          onChange={handleChange}
+        />
+      </Column>
 
-        <Column width={150} sortable resizable>
-          <HeaderCell>Period</HeaderCell>
-          <EditableSelectCell
-            rowData
-            dataKey="period"
-            type="select"
-            onChange={handleChange}
-          />
-        </Column>
+      <Column width={150} sortable resizable>
+        <HeaderCell>Period</HeaderCell>
+        <EditableSelectCell
+          rowData
+          dataKey="period"
+          type="select"
+          onChange={handleChange}
+        />
+      </Column>
 
-        <Column width={200} sortable resizable>
-          <HeaderCell>Status</HeaderCell>
-          <EditableSelectCell
-            rowData
-            dataKey="status"
-            type="select"
-            onChange={handleChange}
-          />
-        </Column>
+      <Column width={200} sortable resizable>
+        <HeaderCell>Status</HeaderCell>
+        <EditableSelectCell
+          rowData
+          dataKey="status"
+          type="select"
+          onChange={handleChange}
+        />
+      </Column>
 
-        <Column width={80} align="left">
-          <HeaderCell>...</HeaderCell>
-          <EditActionCell
-            rowData
-            dataKey="edit"
-            onEdit={handleEdit}
-            onSave={handleSave}
-          />
-        </Column>
-      </Table>
-      <Button
-        onClick={() => {
-          const fakemember = {
-            name: "askdjh",
-            email: " ALSDHSJALK",
-          };
-          let dataCopy = dbData.map((x) => x);
-          dataCopy.push(fakemember);
-          setData(dataCopy);
-        }}
-      >
-        Hej
-      </Button>
-    </>
+      <Column width={80} align="left">
+        <HeaderCell>...</HeaderCell>
+        <EditActionCell
+          rowData
+          dataKey="edit"
+          onEdit={handleEdit}
+          onSave={handleSave}
+        />
+      </Column>
+    </Table>
   );
 };
 
